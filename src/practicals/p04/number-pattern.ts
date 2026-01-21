4967627eaa52b5543f2959e2a572ed47d89a2617
Channarong <channarongnakkhantong@gmail.com>
2026-01-21 16:51:45 +0700

    m

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -3,8 +3,12 @@ if (isNaN(n) || n < 1 || !Number.isInteger(n)) {
     console.log("Invalid Input")
 } else {
 
-    for (let i = n; i > 0; i--) {
-        let line = ""
+    for (let i = 0; i < n+1; i++) {
+        let line = '';
+        
+        for (let s = 0; s < i; s++) {
+          line += ' ';
+        }
         for (let j = i; j > 0; j--) {
             line += j;
         }
