bb67f166a1ec95476b34d1cb51d11908d78e5cae
Channarong <channarongnakkhantong@gmail.com>
2026-01-21 16:09:39 +0700

    a

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -7,13 +7,13 @@ else{
     for(let i = 1;i<=n;i++){
         if(i % 3 ===0 && i % 5 ===0){
             console.log("FizzBuzz");
-        } else if(i % 3 === 0){
+        }else if(i % 3 === 0){
             console.log('Fizz');
         }else if(i % 5 === 0){
             console.log('Buzz');
         }
         else{
-            console.log(`${i}`);
+            console.log([i]);
         }
         }
     }
